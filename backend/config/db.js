c09b5120c3cbbf8e4bf1ccf8e200d9b6b7d096fd
gourav <gouravchourasiya2005@gmail.com>
2026-07-20 15:33:02 +0530

    Fix db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,21 +12,30 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
           serverSelectionTimeoutMS: 5000,
         });
+
         console.log("MongoDB Connected:", conn.connection.host);
+
       } catch (err) {
         console.error("MongoDB Connection Error, retrying in 5 seconds...", err);
+
         setTimeout(connectWithRetry, 5000);
       }
     };
 
-    mongoose.connection.on('disconnected', () => {
-      console.log('MongoDB disconnected! Attempting to reconnect...');
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB disconnected! Attempting to reconnect...");
     });
 
-    mongoose.connection.on('reconnected', () => {
-      console.log('MongoDB reconnected!');
+    mongoose.connection.on("reconnected", () => {
+      console.log("MongoDB reconnected!");
     });
 
     await connectWithRetry();
+
+  } catch (err) {
+    console.error("MongoDB Fatal Error:", err);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
